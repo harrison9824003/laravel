4c20ab97eff9b20eaa0b9877f8402818f4510987
root <admin@example.com>
2022-09-27 22:35:57 +0800

    add api resource

--- project/app/Models/Shop/Product.php
+++ project/app/Models/Shop/Product.php
@@ -2,15 +2,14 @@
 
 namespace App\Models\Shop;
 
-use App\Models\DataType;
-use App\Models\MyImage;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Traits\HasModelId;
+use App\Traits\HasFrontData;
 
 class Product extends Model
 {
-    use HasFactory,HasModelId;
+    use HasFactory,HasModelId,HasFrontData;
 
     protected $table = 'pj_product';
     
@@ -26,9 +25,9 @@ class Product extends Model
     ];
 
     public function images()
-    {
-        $images = MyImage::where('item_id', $this->id)->where('data_id', $this->get_model_id())->get();
-        return $images;
+    {        
+        $model_app = app(\App\Models\Shop\Product::class);
+        return $this->hasMany(\App\Models\Shop\ProductImage::class, 'item_id', 'id')->where('data_id', $model_app->get_model_id());
     }
 
     public function category() {
@@ -37,5 +36,39 @@ class Product extends Model
 
     public function specs() {
         return $this->hasMany(\App\Models\Shop\ProductSpec::class, 'product_id', 'id');
+    }
+
+    /**
+     * 回傳前台統一格式
+     * 
+     * category: 網站總分類
+     * title: 資料標題
+     * sub_title: 資料副標題
+     * create: 建立時間
+     * update: 建立時間
+     * create_person: 建立人員
+     * update_person: 修改人員
+     * content: 主內容
+     * simple_content: 簡介內容
+     * other: 其他內容 e.g. 商品的規格、分類等, 由頁面本身撈取
+    */
+    public function get_front_data() {
+
+        return [
+            'category' => strip_tags($this->category->name),
+            'title' => strip_tags($this->name),
+            'sub_title' => null,
+            'create' => date("Y-m-d", strtotime($this->created_at)),
+            'update' =>  date("Y-m-d", strtotime($this->updated_at)),
+            'create_person' => null,
+            'update_person' => null,
+            'content' => $this->intro,
+            'simple_content' => strip_tags($this->simple_intro),
+            'other' => [
+                'images' => $this->images,
+                'specs' => $this->specs
+            ]
+        ];
+
     }
 }
--- project/routes/api.php
+++ project/routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::name('front.')->prefix('front')->group(function(){   
+
+    // 顯示單一筆資料
+    Route::get('/{front}', [\App\Http\Controllers\Api\FrontController::class, 'show'])->name('show');
+
+    // 顯示單一類別列表
+    Route::get('/category/{category}', [\App\Http\Controllers\Api\FrontController::class, 'category'])->name('category');
+});
