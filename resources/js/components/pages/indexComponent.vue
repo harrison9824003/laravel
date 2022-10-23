67f777ef3136658da06908cccba78387537fb935
root <admin@example.com>
2022-10-23 12:23:30 +0800

    update

---
+++ resources/js/components/pages/indexComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <SliderComponent></SliderComponent>
+        <ContentComponent></ContentComponent>
+        <SetpsComponent></SetpsComponent>
+    </div>    
+</template>
+
+<script>
+import SliderComponent from '../index/sliderComponent.vue'
+import SetpsComponent from '../index/setpsComponent.vue'
+import ContentComponent from '../index/contentComponent.vue'
+export default {
+    mounted() {
+        console.log("index component mounted")
+        
+    },
+    components:{
+        SliderComponent,
+        SetpsComponent,
+        ContentComponent
+    }
+}
+</script>
