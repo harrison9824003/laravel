43e9cde139a387fd1ad44a8cf3dcf84e5437570f
root <admin@example.com>
2022-11-05 01:25:14 +0800

    Revert "update"
    This reverts commit 6b5b8e9a0569e081622ea24e4e35fbaa5310a381.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
     public function index() {
-        return view('admin.index'); 
+        return view('admin.index');
     }
 
     // 刪除全站圖片
