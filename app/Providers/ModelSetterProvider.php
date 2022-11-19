b8f278e3dabed2930cacc085902a1e13b941aad8
root <admin@example.com>
2022-11-19 21:09:25 +0800

    update model provider

--- app/Providers/ModelSetterProvider.php
+++ app/Providers/ModelSetterProvider.php
@@ -14,13 +14,43 @@ use App\Models\Shop\ProductImage;
 class ModelSetterProvider extends ServiceProvider
 {
     private const MODELIDS = [
-        Article::class => '1',
-        Product::class => '2',
+        Article::class => 1,
+        Product::class => 2,
         SpecCategory::class => 3,
         DataType::class => 4,
         Categroy::class => 5,
         DataTypeFolder::class => 6,
         ProductImage::class => 7,
+    ];
+
+    private const MODELROUTE = [
+        Article::class => 'article',
+        Product::class => 'product',
+        SpecCategory::class => 'spec',
+        DataType::class => 'datatype',
+        Categroy::class => 'category',
+        DataTypeFolder::class => 'datatypefolder',
+        ProductImage::class => 'productimg',
+    ];
+
+    private const MODELICON = [
+        Article::class => 'bx-pencil',
+        Product::class => 'bxl-product-hunt',
+        SpecCategory::class => 'bx-category',
+        DataType::class => 'bx-briefcase',
+        Categroy::class => 'bx-category',
+        DataTypeFolder::class => 'bx-folder',
+        ProductImage::class => 'bx-image-alt',
+    ];
+
+    private const MODELNAME = [
+        Article::class => '文章',
+        Product::class => '商品',
+        SpecCategory::class => '規格分類',
+        DataType::class => '模組',
+        Categroy::class => '全站分類',
+        DataTypeFolder::class => '模組分類',
+        ProductImage::class => '商品圖片',
     ];
 
     /**
@@ -87,22 +117,25 @@ class ModelSetterProvider extends ServiceProvider
      */
     public function boot()
     {
-        $checks = DataType::pluck('id')->toArray();
+        if(!DataType::all()->count()){
+            $checks = DataType::pluck('id')->toArray();
 
-        //  檢查綁定的 model 是否有建立 model id 到資料庫
-        foreach (self::MODELIDS as $model_name => $model_id) {
-            if (!in_array($model_id, $checks)) {
-                $datetype = DataType::create([
-                    'id' => $model_id,
-                    'name' => $model_name,
-                    'class_name' => $model_name,
-                    'disabled' => 0,
-                    'icon' => 'bx-note',
-                    'folder_id' => 0,
-                    'router_path' => 'adm'
-                ]);
-                //$datetype->save();
+            //  檢查綁定的 model 是否有建立 model id 到資料庫
+            foreach (self::MODELIDS as $model_name => $model_id) {
+                if (!in_array($model_id, $checks)) {
+                    $datetype = DataType::create([
+                        'id' => $model_id,
+                        'name' => self::MODELNAME[$model_name],
+                        'class_name' => $model_name,
+                        'disabled' => 0,
+                        'icon' => isset(self::MODELICON[$model_name]) ? self::MODELICON[$model_name] : 'bx-note',
+                        'folder_id' => 0,
+                        'router_path' => 'adm/'. self::MODELROUTE[$model_name]
+                    ]);
+                    //$datetype->save();
+                }
             }
         }
+        
     }
 }
